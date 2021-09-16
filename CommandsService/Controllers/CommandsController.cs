c76229bf7b7551141be0f5fd3076253380bb6eb2
Dan Deng <hui_danny_deng@hotmail.com>
2021-09-16 15:07:09 -0400

    add Commands controller

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    public class CommandsController : ControllerBase
+    {
+
+    }
+}
--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -5,11 +5,12 @@ using CommandsService.Data;
 using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CommandsServices.Controllers
+namespace CommandsService.Controllers
 {
-    [Route("api/c/{controller}")]
+    [Route("api/c/[controller]")]
     [ApiController]
-    public class PlatformsController: ControllerBase{
+    public class PlatformsController : ControllerBase
+    {
         private readonly ICommandRepo _repository;
 
         public IMapper _mapper { get; }
@@ -24,7 +25,7 @@ namespace CommandsServices.Controllers
         public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatform()
         {
             Console.WriteLine("---> Getting Platform for Command Service");
-            var platformItems=_repository.GetAllPlatform();
+            var platformItems = _repository.GetAllPlatform();
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
 
         }
